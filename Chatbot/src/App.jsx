7547d235a9b3e0f2da9715e9a9ac11377cbee19d
ManjunathR <manjunath@nuvepro.com>
2023-12-17 22:00:58 +0530

    added network

--- Chatbot/src/App.jsx
+++ Chatbot/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
 
   async function fetchData() {
     try {
-      const response = await fetch("http://localhost:5000/dummy");
+      const response = await fetch("http://http://nped1uuxr.cloudloka.com/api");
 
       // Check if the request was successful (status code 200-299)
       if (!response.ok) {
